a0f2bb5e5768f5a8480d99acd49517be37ff1089
Kirill <kirillteplycov888@gmail.com>
2023-11-22 21:10:06 +0500

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PrintManagementSystem_Тепляков
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void EditOperation(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteOperation(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SelectedType(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void SelectedFormat(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void textBoxCount_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+
+        }
+
+        private void AddOperation(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ColorsChange(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
