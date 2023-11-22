6a207cce8f847e3ad1142ad801c4efe7edd4f5ed
Kirill <kirillteplycov888@gmail.com>
2023-11-22 21:21:43 +0500

    Реализованные классы. Тепляков

---
+++ Classes/Format.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintManagementSystem_Тепляков.Classes
+{
+    public class Format
+    {
+        public int id {  get; set; }
+        public string format { get; set; }
+        public string description { get; set; }
+
+        public Format(int _id, string _format, string _description)
+        {
+            this.id = _id;
+            this.format = _format;
+            this.description = _description;
+        }
+
+        public static List<Format> AllFormats()
+        {
+            List<Format> allFormat = new List<Format>();
+            allFormat.Add(new Format(1, "А4", ""));
+            allFormat.Add(new Format(2, "А3", ""));
+            allFormat.Add(new Format(3, "А2", ""));
+            allFormat.Add(new Format(4, "А1", ""));
+            return allFormat;
+        }
+    }
+}
---
+++ Classes/TypeOpertation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintManagementSystem_Тепляков.Classes
+{
+    public class TypeOpertation
+    {
+        public int id { get; set; }
+        public string format { get; set; }
+        public string description { get; set; }
+
+        public TypeOpertation(int _id, string _format, string _description)
+        {
+            this.id = _id;
+            this.format = _format;
+            this.description = _description;
+        }
+
+        public static List<TypeOpertation> AllTypeOpertation()
+        {
+            List<TypeOpertation> allTypeOpertation = new List<TypeOpertation>();
+            allTypeOpertation.Add(new TypeOpertation(1, "Печать", ""));
+            allTypeOpertation.Add(new TypeOpertation(2, "Копия", ""));
+            allTypeOpertation.Add(new TypeOpertation(3, "Сканирование", ""));
+            allTypeOpertation.Add(new TypeOpertation(4, "Ризограф", ""));
+            return allTypeOpertation;
+        }
+    }
+}
